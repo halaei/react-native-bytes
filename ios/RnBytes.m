69c76ba3c00ca6ebca3b6990b467fb343e89dfe0
hamid <halaeiv@gmail.com>
2021-01-17 14:01:24 +0330

    ios filesize...

--- ios/RnBytes.m
+++ ios/RnBytes.m
@@ -5,10 +5,35 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(getFileLength:(NSString *)path
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    @try {
+        NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:URL error:&attributesError];
+        NSNumber *fileSize = [fileAttributes objectForKey:NSFileSize];
+        resolve(fileSize);
+    } @catch (NSException *exception) {
+        reject(exception)
+    }
 }
+
+RCT_EXPORT_METHOD(readFromAndWriteTo:(NSString *)sourcePath
+                  targetPath(NSString *)targetPath
+                  overwrite(BOOL)overwrite
+                  append(BOOL)append
+                  from(NSNumber *)from
+                  to(NSNumber *)to
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    if (!overwrite || append) {
+        reject(@"append not supported");
+        return;
+    }
+    reject(@"todo");
+}
+
+
 
 @end
